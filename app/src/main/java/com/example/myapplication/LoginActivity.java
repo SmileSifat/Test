a2c65b05d76471f7f06f095383f7b771852def96
SmileSifat <sifatabdullah386@gmail.com>
2021-12-27 15:14:29 +0600

    4th

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -3,12 +3,24 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText UserName,PassWord;
+    Button Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        UserName=findViewById(R.id.userNameEditText);
+        PassWord=findViewById(R.id.passwordEditText);
+        Login=findViewById(R.id.loginButton);
+
+
     }
 }
---
+++ app/src/main/java/com/example/myapplication/LoginResponse.java
@@ -0,0 +1,40 @@
+package com.example.myapplication;
+
+public class LoginResponse {
+    private int user_id;
+    private String email;
+    private String username;
+
+    public LoginResponse() {
+    }
+
+    public LoginResponse(int user_id, String email, String username) {
+        this.user_id = user_id;
+        this.email = email;
+        this.username = username;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
